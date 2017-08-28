98555dcff67edaac26a7f41f81fa1ef948fc43f7
Julianne Quinn <julianneq@users.noreply.github.com>
2017-08-28 16:01:34 -0400

    Update KNN_sampling.m

--- stationary_generator/KNN_sampling.m
+++ stationary_generator/KNN_sampling.m
@@ -1,15 +1,21 @@
 function [py, yearID] = KNN_sampling( KNN_id, indices, Wcum, Qdaily, month )
 
-% py = KNN_sampling( KKN_id, Wcum, P )
+% py = KNN_sampling( KKN_id, indices, Wcum, Qdaily, month )
 %
 % Selection of one KNN according to the probability distribution defined by
 % the weights W.
 %
 % Input:    KNN_id = indices of the first K-nearest neighbors
+%           indices = n x 2 matrix where n is the number of monthly totals
+%             and the 2 columns store the historical year in which each
+%             monthly total begins, and the number of shift index
+%             where 1 is 7 days earlier and 15 is 7 days later
 %           Wcum = cumulated probability for each nearest neighbor
-%           P = proportion matrix
+%           Qdaily = historical data
+%           month = month being disaggregated
 % Output:   py = selected proportion vector corresponding to the sampled
-%           year
+%             shifted historical month
+%           yearID = randomly selected monthly total (row to select from indices)
 %
 % MatteoG 31/05/2013
 
